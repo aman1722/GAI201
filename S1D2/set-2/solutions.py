bf414b320393aee2452b728e7bf435e18c21db9c
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-08-05 23:12:37 +0530

    set

--- S1D2/set-2/solutions.py
+++ S1D2/set-2/solutions.py
@@ -42,3 +42,54 @@ for num in numbers:
 
 
 
+
+'''
+Problem **3: Append new string in the middle of a given string**
+
+Given two strings, `s1` and `s2`. Write a program to create a new string `s3` by appending `s2` in the middle of `s1`.
+
+Input:-
+s1 = "Ault"
+s2 = "Kelly"
+
+Output:-
+AuKellylt
+
+'''
+
+
+def appendMiddle(s1,s2):
+    middle = len(s1)//2
+    s3=s1[:middle]+s2+s1[:middle]
+    print(s3)
+
+
+
+
+'''
+### Problem **4: Arrange string characters such that lowercase letters should come first**
+
+Given string contains a combination of the lower and upper case letters. Write a program to arrange 
+the characters of a string so that all lowercase letters should come first.
+
+Input:-
+str1 = PyNaTive
+
+Output:-
+yaivePNT
+'''
+
+def arrangeLowerFirst(s):
+    lowerChars = [char for char in s if char.islower()]
+    upperChars = [char for char in s if char.isupper()]
+    ans = "".join(lowerChars + upperChars)
+    print(ans)
+
+
+
+
+
+
+
+
+
