e11ba72606121b9c71ad3ab0a452a78ed43ce20f
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-08-07 11:25:12 +0530

    ssolutions

---
+++ S1D2/Set-3/solution.py
@@ -0,0 +1,85 @@
+##1
+data = [("John", 25), ("Jane", 30)]
+output = [f"{name} is {age} years old." for name, age in data]
+output = " ".join(output)
+print(output)
+
+##2
+
+
+
+
+##3
+def two_sum(nums, target):
+    num_to_index = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], i]
+        num_to_index[num] = i
+    return []
+nums = [2, 7, 11, 15]
+target = 9
+result = two_sum(nums, target)
+print(result)
+
+
+##4
+input_string = "madam"
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")  # Convert to lowercase and remove spaces
+    return s == s[::-1]
+if is_palindrome(input_string):
+    print(f"The word {input_string} is a palindrome.")
+else:
+    print(f"The word {input_string} is not a palindrome.")
+
+##5
+# from queue import Queue
+# class StackUsingQueue:
+#     def _init_(self):
+#         self.queue1 = Queue()
+#         self.queue2 = Queue()
+#     def push(self, value):
+#         self.queue1.put(value)
+#     def pop(self):
+#         if self.queue1.empty():
+#             return None
+#         while self.queue1.qsize() > 1:
+#             self.queue2.put(self.queue1.get())
+#         popped_value = self.queue1.get()
+#         self.queue1, self.queue2 = self.queue2, self.queue1  # Swap the queues
+#         return popped_value
+# stack = StackUsingQueue()
+# stack.push(1)
+# stack.push(2)
+# print(stack.pop())
+# stack.push(3)
+# print(stack.pop())
+# print(stack.pop())
+
+
+
+##6
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+##7
+arr = [64, 25, 12, 22, 11]
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+selection_sort(arr)
+print(arr)
+
--- S1D2/set-2/solutions.py
+++ S1D2/set-2/solutions.py
@@ -1,13 +1,13 @@
-#Problem **1: Print the following pattern**
-#Write a program to print the following number pattern using a loop.
-'''
-1
-1 2
-1 2 3
-1 2 3 4
-1 2 3 4 5
-'''
-# Solution:-
+# #Problem 1: Print the following pattern
+# #Write a program to print the following number pattern using a loop.
+# # '''
+# # 1
+# # 1 2
+# # 1 2 3
+# # 1 2 3 4
+# # 1 2 3 4 5
+# # '''
+# # # Solution:-
 
 n = 5  
 
@@ -15,21 +15,10 @@ for i in range(1, n + 1):
     for j in range(1, i + 1):
         print(j, end=" ")
     print()
-
-
-
-'''
-### Problem **2: Display numbers from a list using loop**
-
-Write a program to display only those numbers from a [list](https://pynative.com/python-lists/) that satisfy the following conditions
-
-- The number must be divisible by five
-- If the number is greater than 150, then skip it and move to the next number
-- If the number is greater than 500, then stop the loop
-'''
-
 #Solution
 
+
+# ##2
 numbers = [12, 75, 150, 180, 145, 525, 50]
 
 for num in numbers:
@@ -41,55 +30,90 @@ for num in numbers:
         print(num)
 
 
+##3
+def append_middle(s1, s2):
+    middle = len(s1) // 2  # Calculate the middle index
+    s3 = s1[:middle] + s2 + s1[middle:]  # Combine the strings
+    return s3
 
-
-'''
-Problem **3: Append new string in the middle of a given string**
-
-Given two strings, `s1` and `s2`. Write a program to create a new string `s3` by appending `s2` in the middle of `s1`.
-
-Input:-
 s1 = "Ault"
 s2 = "Kelly"
+res = append_middle(s1, s2)
+print(res)
 
-Output:-
-AuKellylt
+#4
+def arrange_lower_first(s):
+    lower_chars = [char for char in s if char.islower()]
+    upper_chars = [char for char in s if char.isupper()]
+    arranged_str = ''.join(lower_chars + upper_chars)
+    return arranged_str
 
-'''
+str1 = "ChAnDaN"
+result = arrange_lower_first(str1)
+print(result)
 
+##5
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
 
-def appendMiddle(s1,s2):
-    middle = len(s1)//2
-    s3=s1[:middle]+s2+s1[:middle]
-    print(s3)
+def addlist(list1, list2):
+    newlist = []
+    minlen = min(len(list1), len(list2))
+    for i in range(minlen):
+        newlist.append(list1[i] + list2[i])
+    
+    if len(list1) > minlen:
+        newlist.extend(list1[minlen:])
+    elif len(list2) > minlen:
+        newlist.extend(list2[minlen:])
+    
+    return newlist
 
+res = addlist(list1, list2)
+print(res)
 
+##6
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
 
+def addlist(list1, list2):
+    new_list = [item1 + item2 for item1 in list1 for item2 in list2]
+    return new_list
 
-'''
-### Problem **4: Arrange string characters such that lowercase letters should come first**
+res = addlist(list1, list2)
+print(res)
 
-Given string contains a combination of the lower and upper case letters. Write a program to arrange 
-the characters of a string so that all lowercase letters should come first.
+##7
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+def reverse(list1, list):
+    for item1, item2 in zip(list1,reversed(list2)):
+        print(item1 , item2)
+reverse(list1,list2)
 
-Input:-
-str1 = PyNaTive
+##8
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
 
-Output:-
-yaivePNT
-'''
+add = {employee: defaults.copy() for employee in employees}
+print(add)
 
-def arrangeLowerFirst(s):
-    lowerChars = [char for char in s if char.islower()]
-    upperChars = [char for char in s if char.isupper()]
-    ans = "".join(lowerChars + upperChars)
-    print(ans)
+##9
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New York"
+}
 
+keys = ["name", "salary"]
 
+new_dict = {key: sample_dict[key] for key in keys}
+print(new_dict)
 
-
-
-
-
-
-
+##10
+tuple1 = (11, [22, 33], 44, 55)
+tuple_list = list(tuple1)
+tuple_list[1][0] = 222
+modified_tuple = tuple(tuple_list)
+print(modified_tuple)
