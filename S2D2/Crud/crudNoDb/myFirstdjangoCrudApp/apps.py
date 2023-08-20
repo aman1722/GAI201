fefe45b1a392af56e916399ae269e5344979cb7a
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-08-20 14:30:58 +0530

    assingment complitions

---
+++ S2D2/Crud/crudNoDb/myFirstdjangoCrudApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyfirstdjangocrudappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myFirstdjangoCrudApp'
---
+++ S2D2/Crud/greetings/Hellogreetings/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HellogreetingsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Hellogreetings'
